6d630e60c0539c7c1c35fdce5f75f20188fe594a
Mack McConnell <mackmcconnell@Macks-iMac.local>
2014-08-02 19:21:14 +0200

    Change email title

--- app/mailers/cute_mailer.rb
+++ app/mailers/cute_mailer.rb
@@ -2,6 +2,6 @@ class CuteMailer < ActionMailer::Base
   default :from => "mackmcconnell@gmail.com"
 
   def cute_email(email)
-    mail to: email, reply_to: 'mackmcconnell@gmail.com', subject: "Support"
+    mail to: email, reply_to: 'mackmcconnell@gmail.com', subject: "2Cute"
   end
 end
