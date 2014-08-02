2f7be48d463a86017d669e6317123b527cfbe965
Mack McConnell <mackmcconnell@Macks-iMac.local>
2014-08-02 20:49:44 +0200

    Add new possible subject lines

--- app/mailers/cute_mailer.rb
+++ app/mailers/cute_mailer.rb
@@ -2,6 +2,12 @@ class CuteMailer < ActionMailer::Base
   default :from => "mackmcconnell@gmail.com"
 
   def cute_email(email)
-    mail to: email, reply_to: 'mackmcconnell@gmail.com', subject: "2Cute"
+    mail to: email, reply_to: 'mackmcconnell@gmail.com', subject: subject
+  end
+
+  private
+
+  def subject
+    ["2Cute", "Too much Cuteness", "Cute overload", "Cute is the new Black", "Can Haz Cute?"].sample
   end
 end
